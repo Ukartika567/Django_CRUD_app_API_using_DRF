e0c57a37908d88ebb7e6a876bb2f44febd403bda
Usha <ushacec1234@gmail.com>
2021-08-28 09:43:55 -0700

    update heroku

--- crudproj/settings.py
+++ crudproj/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -131,6 +131,7 @@ REST_FRAMEWORK={
     'DEFAULT_RENDERER_CLASSES':('rest_framework.renderers.JSONRenderer',)
 }
 
+django_heroku.settings(locals())
 
 
 
