f6500c4561927d06b6871571029c8491c6a8be0e
Usha Yadav <84615461+Ukartika567@users.noreply.github.com>
2021-08-29 00:52:44 -0700

    updated

--- crudapp/admin.py
+++ crudapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import EmployeeDetails
 # Register your models here.
+
+@admin.register(EmployeeDetails)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display=['id', 'emp_name', 'emp_email', 'emp_password']
